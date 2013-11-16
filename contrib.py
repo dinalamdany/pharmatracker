685fc673c93f18785bd29a3a56ad15402a76ef21
Dina Lamdany <dinalamdany@gmail.com>
2013-11-16 17:30:09 -0500

    better code

--- contrib.py
+++ contrib.py
@@ -16,33 +16,31 @@ with open('contributions.csv','rU') as csvfile:
         if row[25] == '':
                 pass
         elif row[25][0] == 'N':
-            try:
+            if row[24] in names_to_money:
                 money = names_to_money[row[24]]
                 money += abs(float(row[7]))
                 names_to_money[row[24]] = money
-            except:
+            else:
                 names_to_money[row[24]] = abs(float(row[7]))
         elif row[25][0] == 'C':
-            try:
+            if row[25] in names_to_money:
                 money = fec_to_money[row[25]]
                 money += abs(float(row[7]))
                 fec_to_money[row[25]] = money
-            except:
+            else:
                 fec_to_money[row[25]] = abs(float(row[7]))
 
 ie_to_money= dict()
-for x in recipients.keys():
-    try:
-        money = fec_to_money[x]
-        id = recipients[x]
+for fec in recipients.keys():
+    if fec in fec_to_money:
+        money = fec_to_money[fec]
+        id = recipients[fec]
         ie_to_money[id] = money
-    except:
-        pass
 names = open('names_to_ids.json')
 name_to_id = json.load(names)
 for name, ids in name_to_id.iteritems():
    for id in ids:
-       iemoney = ie_to_money.get(x,0)
+       iemoney = ie_to_money.get(id,0)
        names_to_money[name] += iemoney
 
 sortednames = sorted(names_to_money.items(), key=itemgetter(1))
