83a4fbf156f7430fc05689bf28584e19a6a39b3c
Sophie Chou <sbchou@gmail.com>
2013-11-16 16:38:42 -0500

    add axes and hover

--- viz/js/graph.js
+++ viz/js/graph.js
@@ -17,8 +17,18 @@ d3.text("data/fake_data.csv", function(datasetText) {
 
 	var rScale = d3.scale.linear()
 						 .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-						 .range([2, 5]);
+						 .range([2, 10]);
+	//Define X axis
+	var xAxis = d3.svg.axis()
+					  .scale(xScale)
+					  .orient("bottom")
+					  .ticks(5);
 
+	//Define Y axis
+	var yAxis = d3.svg.axis()
+					  .scale(yScale)
+					  .orient("left")
+					  .ticks(5);
 	//Create SVG element
 	var svg = d3.select("body")
 				.append("svg")
@@ -37,8 +47,54 @@ d3.text("data/fake_data.csv", function(datasetText) {
 	   })
 	   .attr("r", function(d) {
 	   		return rScale(d[1]);
-	   });
-	   
+		})
+	   .style("fill", "pink")
+
+	   .on("mouseover", function(d) {
+	   		//highlight color
+	   		d3.select(this).style("fill", "aliceblue");
+			//Get this bar's x/y values, then augment for the tooltip
+			var xPosition = parseFloat(d3.select(this).attr("cx"));
+			var yPosition = parseFloat(d3.select(this).attr("cy"));
+
+			svg.append("text")
+			   .attr("id", "tooltip")
+			   .attr("x", xPosition)
+			   .attr("y", yPosition)
+			   .attr("text-anchor", "middle")
+			   .attr("font-family", "sans-serif")
+			   .attr("font-size", "11px")
+			   .attr("font-weight", "bold")
+			   .attr("fill", "red")
+			   .text(d.join());
+
+
+		})
+		.on("mouseout", function() {
+			//unhighlight
+			d3.select(this).style("fill", "pink");
+			//Remove the tooltip
+			d3.select("#tooltip").remove();
+			
+		});
+
+			
+		//Create X axis
+		svg.append("g")
+			.attr("class", "axis")
+			.attr("transform", "translate(0," + (h - padding) + ")")
+			.call(xAxis);
+		
+		//Create Y axis
+		svg.append("g")
+			.attr("class", "axis")
+			.attr("transform", "translate(" + padding + ",0)")
+			.call(yAxis);
+});
+
+
+
+/*
 	svg.selectAll("text")
 	   .data(dataset)
 	   .enter()
@@ -55,4 +111,6 @@ d3.text("data/fake_data.csv", function(datasetText) {
 	   .attr("font-family", "sans-serif")
 	   .attr("font-size", "11px")
 	   .attr("fill", "red");
+
 });
+*/
