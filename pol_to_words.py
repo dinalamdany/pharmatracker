f50986b46a1b61d9844a664fce59021de4579993
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2013-11-16 14:16:22 -0500

    initial heap code

--- pol_to_words.py
+++ pol_to_words.py
@@ -1,6 +1,8 @@
 from secrets import Secrets
 
-import httplib
+import urllib2 
+import json
+import heapq
 
 class PoliticiansToWords:
     api_key = Secrets.sunlight_key
@@ -9,9 +11,26 @@ class PoliticiansToWords:
     def get_words_from_politician(cls, politician_id):
         request_uri = 'http://capitolwords.org/api/1/phrases.json?entity_type=legislator&entity_value=' + str(politician_id) + '&apikey=' + str(cls.api_key)
 
-        return "test"
+        response = urllib2.urlopen(request_uri)
+        words = json.loads(response.read())
+
+        return [(word['count'], word['ngram']) for word in words]
+
+    # returns top N words from total of politician ids
+    def top_words(cls, politician_ids, n=20):
+        words = dict()
+
+        for pol_id in politician_ids:
+            top_pol = PoliticianToWords.get_words_from_politician(pol_id)
+
+            for count, word in top_pol:
+                words[word] = count if not words[word] else count + words[word]     
     
+
+        top = heapify([(count, word) for count, word in words])
+
+        return heapq.nlargest(n, top)
 
      
      
-print PoliticiansToWords.get_words_from_politician('test')
+PoliticiansToWords.get_words_from_politician('L000551')
