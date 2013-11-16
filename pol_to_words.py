88434fd09724929d41ab506172d50a02e170c84f
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2013-11-16 18:21:54 -0500

    YTFD

--- pol_to_words.py
+++ pol_to_words.py
@@ -1,4 +1,5 @@
 from secrets import Secrets
+import contrib as Contrib
 
 import urllib2 
 import json
@@ -46,6 +47,11 @@ class PoliticiansToWords:
             for count, word in top_pol:
                 words[word] = count + words.get(word, 0)
 
+        blacklist = ['going', 'think', 'percent', 'floor', 'friend', 'leader', 'side', 'court', 'lot', 'really', 'gentlewoman', 'san', 'actually', 'fact', 'ought', 'talking', 'basically', 'drugs', 'taxes', 'republican', 'democrats']
+        for word in blacklist:
+            if word in words:
+                words.pop(word)
+
         top = [(count, word) for word, count in words.iteritems()]
         heapq.heapify(top)
 
@@ -62,10 +68,26 @@ def id_to_bioid():
     mapping = dict()
 
     with open('bioguide_to_ie.csv', 'rb') as csvfile:
-        reader = csv.reader(csvfile, delimeter=',')
+        reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
             mapping[row[1]] = row[0]
 
     return mapping
 
-print PoliticiansToWords.top_words(['A000022','L000551'], 0)
+id_list = Contrib.get_sorted_ids()
+id_list = [x for x,y in id_list[-53:-3]]
+id_list = [y for x in id_list for y in x]
+
+bioids = set()
+mapping = id_to_bioid()
+for id in id_list:
+    if id in mapping:
+        bioids.add(mapping[id])
+    else:
+        pass
+
+
+words = PoliticiansToWords.top_words(list(bioids), 50)
+#took out more words
+words = ['health', 'tax', ' president', 'care', 'medicare', 'consent', 'insurance', 'unanimous', 'budget', 'drug', 'energy', 'rights', 'law', 'majority', 'education', 'seniors', 'oil', 'prescription', 'nuclear', 'patients', 'plan', 'debt', 'research', 'water', 'cancer', 'benefit']
+
